24cd90e2798cf8878ab5f89ada3ad46d9f3bedd7
Moenupa WANG <mineworldy@outlook.com>
2021-04-04 19:51:45 +0800

    run_cmd method Update

--- src/lib/cli.c
+++ src/lib/cli.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 #define RETURN_CMD_STR(val, e) {if (val == e) {return #e;}}
 #define RETURN_CMD_INT(val, e) {if (strcmp(val, #e) == 0) {return e;}}
@@ -7,11 +7,36 @@
 // typedef enum {INVALID, CMD1, CMD2, CMD3, ...} CMD;
 typedef enum {INVALID = 0, addMeeting, addPresentation, addConference, bookDevice, addBatch, printBookings, endProgram} CMD;
 
-char *cmd_to_string(int value);
+char *cmd_to_string(int cmd);
 int cmd_to_int(char *cmd);
+
+int run_cmd(int cmd)
+{
+    switch (cmd)
+    {
+    case addMeeting        :
+    case addPresentation   :
+    case addConference     :
+    case bookDevice        :
+    case addBatch          :
+    case printBookings     :
+        /* code */
+        return 1;
+    case endProgram        :
+        return 0;
+    case INVALID           :
+        // cmd parsing is wrong
+        return -2;
+
+    default:
+        // cmd (string) -> (int) parsing error!
+        return -1;
+    }
+}
 
 int main(int argc, char **argv)
 {
+    printf("---- DEBUG START ----\n");
     printf("received %d args in total,", argc);
 
     for (int i = 0; i < argc; i++)
@@ -19,14 +44,23 @@ int main(int argc, char **argv)
         printf(" \"%s\"", argv[i]);
     }
     printf("\n");
-    printf("%s", cmd_to_string(addMeeting));
-    printf("\n");
-    printf("%d", cmd_to_int("addMeeting"));
-    printf("\n");
+    printf("cmd in string: %s, cmd in int: %d\n", cmd_to_string(addMeeting), cmd_to_int("addMeeting"));
+    printf("---- DEBUG END ----\n");
 
+    int loop = 1;
+    while (loop != 0)
+    {
+        scanf("%d", &loop);
+        printf("input @%d, ", loop);
+
+        loop = run_cmd(loop);
+        printf("execute result @%d\n", loop);
+    }
+    printf("quit loop, exiting main program\n");
 }
 
-char* cmd_to_string(int cmd) {
+char* cmd_to_string(int cmd)
+{
     RETURN_CMD_STR( cmd     , addMeeting        );
     RETURN_CMD_STR( cmd     , addPresentation   );
     RETURN_CMD_STR( cmd     , addConference     );
@@ -37,7 +71,8 @@ char* cmd_to_string(int cmd) {
     RETURN_CMD_STR( INVALID , INVALID           );
 }
 
-int cmd_to_int(char *cmd) {
+int cmd_to_int(char *cmd)
+{
     RETURN_CMD_INT( cmd     , addMeeting        );
     RETURN_CMD_INT( cmd     , addPresentation   );
     RETURN_CMD_INT( cmd     , addConference     );
