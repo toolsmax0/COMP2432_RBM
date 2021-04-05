095788cd29bfa475fd7e59b497e77f25a3cbc728
Moenupa WANG <mineworldy@outlook.com>
2021-04-05 22:52:59 +0800

    CLI body comments added; minor improvements

--- src/lib/cli.c
+++ src/lib/cli.c
@@ -4,29 +4,34 @@
 #define MAX_CMD_LENGTH 20
 #define MAX_PARAM_LENGTH 100
 
-#define RUN_CMD_PARSING_ERROR -2
-#define RUN_CMD_INVALID -1
-#define RUN_CMD_EXIT 0
-#define RUN_CMD_SUCCESS 1
-#define RUN_CMD_RUNTIME_ERROR 2
+// run status flags
+// <0 means error occured
+// =0 means exit system
+// >0 means successfule
+#define RUN_ERROR_PARAM -4
+#define RUN_ERROR_RUNTIME -3
+#define RUN_ERROR_PARSING -2
+#define RUN_ERROR_INVALID_CMD -1
+#define RUN_EXIT 0
+#define RUN_SUCCESS 1
 
 // !!! WARNING !!! NEVER USE these two macro when variable cmd is not defined
 #define RETURN_CMD_STR(e) {if (cmd == e) {return #e;}}
 #define RETURN_CMD_INT(e) {if (strcmp(cmd, #e) == 0) {return e;}}
 
-#define FOREACH_CMD(FUNCTION)       \
-        FUNCTION(INVALID        )   \
-        FUNCTION(addMeeting     )   \
-        FUNCTION(addPresentation)   \
-        FUNCTION(addConference  )   \
-        FUNCTION(bookDevice     )   \
-        FUNCTION(addBatch       )   \
-        FUNCTION(printBookings  )   \
-        FUNCTION(endProgram     )   \
-
-#define GEN_ENUM(ENUM) ENUM,
+// for-each macro, defines all command heads
+#define FOREACH_CMD(f)       \
+        f(INVALID        )   \
+        f(addMeeting     )   \
+        f(addPresentation)   \
+        f(addConference  )   \
+        f(bookDevice     )   \
+        f(addBatch       )   \
+        f(printBookings  )   \
+        f(endProgram     )   \
 
 // typedef enum {INVALID, CMD1, CMD2, CMD3, ...} CMD;
+#define GEN_ENUM(e) e,
 typedef enum {FOREACH_CMD(GEN_ENUM)} CMD;
 
 char* cmd_to_string(int cmd);
@@ -46,43 +51,55 @@ int run_cmd(int cmd, char* param)
     case addBatch           :
     case printBookings      :
         /* code */
-        return RUN_CMD_SUCCESS;
+        // if param not correct,
+        return RUN_ERROR_PARAM;
+        // return RUN_SUCCESS;
 
-    case endProgram         :       return RUN_CMD_EXIT;
-    case INVALID            :       return RUN_CMD_INVALID;
+    case endProgram         :       return RUN_EXIT;
+    case INVALID            :       return RUN_ERROR_INVALID_CMD;
     // cmd (string) -> (int) parsing error!
-    default                 :       return RUN_CMD_PARSING_ERROR;
+    default                 :       return RUN_ERROR_PARSING;
     }
 }
 
 int main(int argc, char** argv)
 {
-    // printf("---- DEBUG START ----\n");
-    // printf("received %d args in total,", argc);
-
-    // for (int i = 0; i < argc; i++)
-    // {
-    //     printf(" \"%s\"", argv[i]);
-    // }
-    // printf("\ncmd in string: %s, cmd in int: %d\n", 
-    //     cmd_to_string(addMeeting), cmd_to_int("addMeeting"));
-    // printf("---- DEBUG END ----\n");
 
     int cmd_int, status;
     char cmd[MAX_CMD_LENGTH], param[MAX_PARAM_LENGTH];
     do
     {
-        scanf("%s %[^;\f\n\r\t\v]", cmd, param);
-        //printf("cmd @\"%s\"/\"%s\", ", cmd, param);
+        scanf("%s %[^;]", cmd, param);
+        // \f\n\r\t\v for normal endings, may be needed when handling a bat file
+        // printf("cmd @\"%s\"/\"%s\", ", cmd, param);
 
         cmd_int = cmd_to_int(cmd);
-        usage(cmd_int);
-        status = run_cmd(cmd_int, param);
-        //printf("status @\"%d\"\n", status);
+        // < 0 then error occured
+        if ((status = run_cmd(cmd_int, param)) < RUN_EXIT)
+        {
+            switch (status)
+            {
+            case RUN_ERROR_PARAM:
+            case RUN_ERROR_INVALID_CMD:
+                // intended, two cases with same handling
+                usage(cmd_int);
+                break;
+            case RUN_ERROR_RUNTIME:
+            case RUN_ERROR_PARSING:
+                puts("this is a bug");
+                break;
+
+            default:
+                puts("Error detected.");
+                break;
+            }
+        }
+        // printf("status @\"%d\"\n", status);
     }
-    while (status != 0);
+    while (status != RUN_EXIT);
 
     printf("quit loop, exiting main program\n");
+
 }
 
 char* cmd_to_string(int cmd)
@@ -110,20 +127,21 @@ const char *SYNTAX[] =
     "  endProgram                                                      ;           \n", //7
     "Parameter Syntax        Information                                           \n", //8
     "  t                       A tenant for booking                                \n", //9
-    "                              tanant_A|tanant_B|tanant_C|tanant_D|tanant_E    \n", //0
-    "  YYYY-MM-DD hh:mm        Booking start time format in ISO 8601               \n", //1
-    "  n.n                     Duration in format hours.minutes                    \n", //2
-    "  p                       Number of people                                    \n", //3
-    "  d                       A device for booking                                \n", //4
-    "                            = webcam_FHD|webcam_UHD|monitor_50|monitor_75     \n", //5
-    "                              |projector_2K|projector_4K|screen_100|screen_150\n", //6
-    "  d d                     Only in two combinations                            \n", //7
-    "                            = webcam_* monitor_*|projector_* screen_*         \n", //8
-    "  f                       File with commands                                  \n", //9
-    "  a                       Algorithms used                                     \n", //0
-    "                            = fcfs|prio|opti|ALL                              \n", //1
+    "                            = tanant_A|tanant_B|tanant_C|tanant_D|tanant_E    \n", //10
+    "  YYYY-MM-DD hh:mm        Booking start time format in ISO 8601               \n", //11
+    "  n.n                     Duration in format hours.minutes                    \n", //12
+    "  p                       Number of people                                    \n", //13
+    "  d                       A device for booking                                \n", //14
+    "                            = webcam_FHD|webcam_UHD|monitor_50|monitor_75     \n", //15
+    "                              |projector_2K|projector_4K|screen_100|screen_150\n", //16
+    "  d d                     Only in two combinations                            \n", //17
+    "                            = webcam_* monitor_*|projector_* screen_*         \n", //18
+    "  f                       File with commands                                  \n", //19
+    "  a                       Algorithms used                                     \n", //20
+    "                            = fcfs|prio|opti|ALL                              \n", //21
 };
 // syntax match-line
+// matches command heads with param body
 const int MATCH[8][11] =
 {
     {21,},
@@ -136,6 +154,9 @@ const int MATCH[8][11] =
     {0,},
 };
 
+
+// generate help msg for each cmd
+// if cmd not valid then print all available msg
 void usage(int cmd)
 {
     printf("\nUsage: \n");
