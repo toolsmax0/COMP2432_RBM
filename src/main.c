09c85dde92cee941991e16fa8be768aad70560e6
Moenupa <mineworldy@outlook.com>
2021-04-15 20:37:08 +0800

    revert main.c changes

--- src/main.c
+++ src/main.c
@@ -5,55 +5,35 @@
 #include <stdlib.h>
 
 #define _DEBUG
-#define _INI_DEBUG
-// #define _REP_DEBUG
-
-#define ANSI_RED "\x1b[31m"
-#define ANSI_GREEN "\x1b[32m"
-#define ANSI_YELLOW "\x1b[33m"
-#define ANSI_BLUE "\x1b[34m"
-#define ANSI_DEFAULT "\x1b[0m"
-
-// global configs
-room rooms[1000];
-device devices[1000];
-device *devices_t[1000];
-int n_devices, n_rooms;
-dictionary *d;
 
 /**
  * @brief initiate all available devices from RBM.ini
  */
-void init_from_ini()
+int init_from_ini()
 {
-// initialize a target, val:variablename
-// val: variablename, f: field, s: ini section
 #define INIT(val, f, s)                                 \
-    int num_##s = iniparser_getsecnkeys(d, #s);         \
-    const char *name_##s[num_##s];                      \
+    int n_##s = iniparser_getsecnkeys(d, #s);           \
+    const char *name_##s[n_##s];                        \
     iniparser_getseckeys(d, #s, name_##s);              \
-    for (int i = 0; i < num_##s; i++) {                 \
+    for (int i = 0; i < n_##s; i++)                     \
+    {                                                   \
         sscanf(name_##s[i], #s ":%s", val[i].name);     \
         val[i].f = iniparser_getint(d, name_##s[i], 0); \
     }
-
-    d = iniparser_load("RBM.ini");
-
-    INIT(devices, quantity, devices);
-    INIT(rooms, capacity, rooms);
-    n_devices = num_devices;
-    n_rooms = num_rooms;
-
-#ifdef _INI_DEBUG
-#define INIT_DEBUG(val, f, s)                        \
+#define _INIT_DEBUG(val, f, s)                      \
     printf("No. of " #s " available: %d\n", n_##s); \
     for (int i = 0; i < n_##s; i++)                 \
         printf("  %d: %s @%d\n", i, val[i].name, val[i].f);
+    dictionary *d = iniparser_load("RBM.ini");
+    INIT(devices, quantity, devices);
+    INIT(rooms, capacity, rooms);
 
-    INIT_DEBUG(devices, quantity, devices);
-    INIT_DEBUG(rooms, capacity, rooms);
+#ifdef _DEBUG
+    _INIT_DEBUG(devices, quantity, devices);
+    _INIT_DEBUG(rooms, capacity, rooms);
 #endif
 
+    return 0;
 }
 
 /**
@@ -63,87 +43,18 @@ void init_from_ini()
  * @param   param   parameters for the command
  * @return  execution status in int, see enum EXE in lib/cmd.h
  */
-EXE run_cmd(int cmd, char *param, request* rq)
+EXE run_cmd(int cmd, char *param)
 {
     // printf("accepted: \"%s\", ", param);
-    int n_param, duration[2];
-    struct tm s; // start time
-
-#define HANDLE_PARAM_ERR if (!rq->isvalid) return RUN_ERROR_PARAM;
-#define SCAN_PARAM_FOR_ADD_FUNCTIONS(rq, s, len)                                        \
-    n_param = sscanf(                                                                   \
-        param, "-%s %d-%d-%d %d:%d %d.%d %d %s %s",                                     \
-        rq->tenant, &(s.tm_year), &(s.tm_mon), &(s.tm_mday), &(s.tm_hour), &(s.tm_min), \
-        &len[0], &len[1], &rq->people, rq->device[0], rq->device[1]);                   \
-    rq->isvalid = (n_param == 11);
-#define SCAN_PARAM_POSTPROCESS(rq, s, len)            \
-    s.tm_year -= 1900; s.tm_mon -= 1; s.tm_sec = 0;   \
-    rq->start = mktime(&s);                           \
-    rq->end = time_after(rq->start, len[0], len[1]);  \
-    rq->roomno = -1;                                  \
-    rq->length = 60 * len[0] + len[1];
+    int n_param;
 
     switch (cmd)
     {
     case addMeeting:
-        SCAN_PARAM_FOR_ADD_FUNCTIONS(rq, s, duration)
-        rq->isvalid |= (n_param == 9);
-        rq->priority = 2;
-        SCAN_PARAM_POSTPROCESS(rq, s, duration)
-
-        HANDLE_PARAM_ERR
-        // addMeeting executions
-        puts("executing addMeeting");
-        break;
-
     case addPresentation:
-        SCAN_PARAM_FOR_ADD_FUNCTIONS(rq, s, duration)
-        rq->priority = 1;
-        SCAN_PARAM_POSTPROCESS(rq, s, duration)
-
-        HANDLE_PARAM_ERR
-        // addPresentation executions
-        puts("executing addPresentation");
-        break;
-
     case addConference:
-        SCAN_PARAM_FOR_ADD_FUNCTIONS(rq, s, duration)
-        rq->priority = 0;
-        SCAN_PARAM_POSTPROCESS(rq, s, duration)
-
-        HANDLE_PARAM_ERR
-        // addConference executions
-        puts("executing addConference");
-        break;
-
     case bookDevice:
-        n_param = sscanf(
-            param, "-%s %d-%d-%d %d:%d %d.%d %s",
-            rq->tenant, &s.tm_year, &s.tm_mon, &s.tm_mday, &s.tm_hour, &s.tm_min,
-            &duration[0], &duration[1], rq->device[0]
-        );
-        rq->device[1][0] = 0;
-        rq->isvalid = (n_param == 9);
-        rq->priority = 4;
-        rq->people = 0;
-        SCAN_PARAM_POSTPROCESS(rq, s, duration)
-
-        HANDLE_PARAM_ERR
-        // bookDevice executions
-        puts("executing bookDevice");
-        break;
-
     case addBatch:
-        char filename[40];
-        n_param = sscanf("-%s", filename);
-        // TODO: fopen(filename) to check whether file exists
-        // the above also affects the following return status RUN_ERROR_PARAM
-        if (n_param != 1) return RUN_ERROR_PARAM;
-        
-        // bookDevice executions
-        puts("executing bookDevice");
-        break;
-
     case printBookings:
         /* code */
         // if param not correct,
@@ -158,207 +69,11 @@ EXE run_cmd(int cmd, char *param, request* rq)
     default:
         return RUN_ERROR_PARSING;
     }
-    return RUN_SUCCESS;
 }
 
-// static consts that should be modified
-static const int MAX_TENANT = 5;
-static const char *TENANTS[] = {
-    "tenant_A", 
-    "tenant_B", 
-    "tenant_C", 
-    "tenant_D", 
-    "tenant_E"
-};
-
-// type of booking in hierarchy (linux style)
-const char *TYPE[] = {
-    "Conference", // 0
-    "Presentation", // 1
-    "Meeting", // 2
-};
-
-#define RQ_ISLAST(arr, i) arr[i].tenant[0] != 0
-#define RQ_ISVALID(arr, i) arr[i].isvalid
-#define ADD_TO_QUEUE(src, queue, counter, src_i, queue_i) queue[queue_i][ counter[queue_i]++ ] = &src[src_i];
-
-void print_booking(request success[], request fail[])
-{
-
-// several constant values
-#define SCSS_HEAD "Date         Start   End     Type          Room     Device\n"
-#define FAIL_HEAD "Date         Start   End     Type          Device\n"
-#define SEP "===========================================================================\n"
-#define END "================================= END =====================================\n"
-
-// several constant functions
-#define FOREACH_TENANT(i, exp) for (int i = 0; i < MAX_TENANT; i++) { exp }
-#define PRINT_DEVICE(cur, offset)                                   \
-    if (cur->device[0][0] == 0) printf("*\n");                      \
-    else if (cur->device[1][0] == 0) printf("%s\n", cur->device[0]);\
-    else printf("%s\n%" #offset "s %s\n", cur->device[0], "", cur->device[1]);
-
-#define SEPERATE(src, dest, counter)                          \
-    for (int i = 0; RQ_ISLAST(src, i); i++) {                 \
-        if (!RQ_ISVALID(src, i)) continue;                    \
-        FOREACH_TENANT(                                       \
-            ti,                                               \
-            if (strcmp(TENANTS[ti], src[i].tenant) == 0)      \
-            { ADD_TO_QUEUE(src, dest, counter, i, ti) break; }\
-    )}
-
-#define PRINT_TIME(cur, sd, st, et)                           \
-    strftime(sd, sizeof(sd), "%F", localtime(&(cur->start))); \
-    strftime(st, sizeof(st), "%R", localtime(&(cur->start))); \
-    strftime(et, sizeof(et), "%R", localtime(&(cur->end)));   \
-    printf("%-12s %-7s %-7s ", sd, st, et);
-
-    // function body begins
-    // seperation queue, seperated into tenants
-    request*queue[1000/MAX_TENANT][MAX_TENANT];
-    // counter to store items in the queue
-    int     counter               [MAX_TENANT];
-    char s_date[15], s_time[6], e_time[6];
-
-    puts(ANSI_GREEN "*** Room Booking - ACCEPTED ***");
-    memset(counter, 0, MAX_TENANT * sizeof(int));
-    SEPERATE(success, queue, counter)
-    FOREACH_TENANT(
-        ti,
-        if (counter[ti] > 0)
-        {
-            printf("\n%s has the following bookings:\n\n" SCSS_HEAD SEP, TENANTS[ti]);
-            for (int i = 0; i < counter[ti]; i++) {
-                request *cur = queue[ti][i];
-                PRINT_TIME(cur, s_date, s_time, e_time)
-
-                if (cur->people != 0)
-                {
-                    printf("%-13s %-8s ", TYPE[ cur->priority ], rooms[ cur->roomno ].name);
-                    PRINT_DEVICE(cur, 51)
-                }
-                else printf("%-13s %-8s %s\n", "*", "*", cur->device[0]);
-            }
-        }
-        else printf("\n%s has no bookings.\n", TENANTS[ti]);
-    )
-    puts("\n" END);
-
-    memset(counter, 0, MAX_TENANT * sizeof(int));
-    SEPERATE(fail, queue, counter)
-    puts(ANSI_RED "*** Room Booking - REJECTED ***");
-    FOREACH_TENANT(
-        ti,
-        if (counter[ti] > 0)
-        {
-            printf("\n%s has the following bookings:\n\n" FAIL_HEAD SEP, TENANTS[ti]);
-            for (int i = 0; i < counter[ti]; i++)
-            {
-                request *cur = queue[ti][i];
-                PRINT_TIME(cur, s_date, s_time, e_time)
-
-                if (cur->people != 0)
-                {
-                    printf("%-13s ", TYPE[ cur->priority ]);
-                    PRINT_DEVICE(cur, 42)
-                }
-                else printf("%-13s %s\n", "*", cur->device[0]);
-            }
-        }
-        else printf("\n%s has no bookings.\n", TENANTS[ti]);
-    )
-    puts("\n" END ANSI_DEFAULT);
-}
-
-void print_perform(request success[], request fail[])
-{
-
-    int n_scss = 0, n_fail = 0, si, fi;
-    for (si = 0; RQ_ISLAST(success, si); si++) if (RQ_ISVALID(success, si)) n_scss++;
-    for (fi = 0; RQ_ISLAST(fail, fi); fi++) if (RQ_ISVALID(fail, fi)) n_fail++;
-    int n_total = si + fi;
-
-    printf( ANSI_BLUE
-        "\tTotal Number of Bookings Received: %-3d (%3.1f%%)\n"
-        "\t      Number of Bookings Assigned: %-3d (%3.1f%%)\n"
-        "\t      Number of Bookings Rejected: %-3d (%3.1f%%)\n",
-        n_total, (float) 100,  
-        n_scss, (float) n_scss/n_total*100,  
-        n_fail, (float) n_fail/n_total*100
-    );
-
-    puts("\n\tUtilizatioion of Time Slot:");
-
-    request*queue_r[1000/n_rooms][n_rooms];
-    int     counter_r             [n_rooms];
-    memset(counter_r, 0, MAX_TENANT * sizeof(int));
-    for (int i = 0; RQ_ISLAST(success, i); i++)
-    {
-        if (!RQ_ISVALID(success, i)) continue;
-        if (0 <= success[i].roomno < n_rooms)
-            ADD_TO_QUEUE(success, queue_r, counter_r, i, success[i].roomno)
-        else
-            puts(ANSI_RED "\tERROR when printing" ANSI_BLUE);
-    }
-
-    request*queue_d[1000/n_devices][n_devices];
-    int     counter_d               [n_devices];
-    memset(counter_d, 0, MAX_TENANT * sizeof(int));
-    for (int i = 0; RQ_ISLAST(success, i); i++)
-    {
-        if (!RQ_ISVALID(success, i)) continue;
-        for (int j = 0; j < n_devices; j++)
-        {
-            if (strcmp(devices[j].name, success[i].device[0]) == 0)
-                ADD_TO_QUEUE(success, queue_d, counter_d, i, j)
-            if (strcmp(devices[j].name, success[i].device[1]) == 0)
-                ADD_TO_QUEUE(success, queue_d, counter_d, i, j)
-        }
-    }
-
-#define PRINT_UTILIZATION(arr, ptr, len, res)                       \
-    for (int di = 0; di < len; di++) {                              \
-        time_t s = -1, e = -1;                                      \
-        int book_len = 0;                                           \
-        for (int j = 0; j < ptr[di]; j++) {                         \
-            request *cur = arr[di][j];                              \
-            s = (s == -1 || s > cur->start) ? cur->start : s;       \
-            e = (e == -1 || e < cur->end) ? cur->end : e;           \
-            book_len += cur->length;                                \
-        } printf("\t      %-20s - %3.1f%%\n",                       \
-            res[di].name, book_len / ((float)(e - s)) * 60 * 100);  \
-    }
-
-    PRINT_UTILIZATION(queue_r, counter_r, n_rooms, rooms)
-    PRINT_UTILIZATION(queue_d, counter_d, n_devices, devices)
-
-    printf("\n\tInvalid requests(s) made: %d\n" ANSI_DEFAULT, n_total-n_scss-n_fail);
-}
-
-#ifdef _REP_DEBUG
-int report_debugger()
-{
-    init_from_ini();
-    request s[7] = {
-        {0, "tenant_E", time(NULL), time(NULL) + 3600, 60, 10, 0, {"monitor_50", "monitor_75"}, 1},
-        {0, "tenant_C", time(NULL), time(NULL) + 3600, 60, 50, 1, {"", ""}, 1},
-        {1, "tenant_A", time(NULL), time(NULL) + 3600, 60, 0, 2, {"projector_2k", ""}, 1},
-        {2, "tenant_B", time(NULL) - 7200, time(NULL) - 3600, 60, 20, 0, {"monitor_50", ""}, 1},
-        {2, "tenant_E", time(NULL) - 3600, time(NULL), 60, 15, 1, {"monitor_75", ""}, 1},
-    };
-    request f[10] = {
-        {0, "tenant_E", time(NULL), time(NULL)+3600, 60,10,0,{"monitor_50","monitor_75"}, 1},
-        {0, "tenant_C", time(NULL), time(NULL)+3600, 60,50,0,{"",""}, 0}, //invalid
-        {1, "tenant_A", time(NULL), time(NULL)+3600, 60,0,0,{"projector_2k",""}, 1},
-        {2, "tenant_B", time(NULL), time(NULL)+3600, 60,20,0,{"monitor_50",""}, 0}, //invalid
-        {2, "tenant_E", time(NULL), time(NULL)+3600, 60,15,0,{"monitor_75",""}, 1},
-        {2, "tenant_E", time(NULL), time(NULL)+3600, 60,15,0,{"monitor_75",""}, 1},
-    };
-    print_booking(s, f);
-    print_perform(s, f);
-    return 0;
-}
-#endif
+room rooms[1000];
+device devices[1000];
+device *devices_t[1000];
 
 int main()
 {
@@ -425,11 +140,8 @@ int main()
             sscanf(input, "%s %[^;]", cmd, param);
 
             cmd_int = cmd_to_int(cmd);
-            request *rq = malloc(sizeof(request));
-            // TODO: add request to timeline
-            
             // < 0 then error occured
-            if ((execution = run_cmd(cmd_int, param, rq)) < RUN_EXIT)
+            if ((execution = run_cmd(cmd_int, param)) < RUN_EXIT)
             {
                 switch (execution)
                 {
