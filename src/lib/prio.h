da3940c2d5d821c654838bd067aafd2be6ee5b85
toolsmax <xingshijichina@outlook.com>
2021-04-19 22:52:57 +0800

    bug fix

--- src/lib/prio.h
+++ src/lib/prio.h
@@ -4,4 +4,4 @@
 
 // schedule the requests using priority, and save the successsul requests in success[], failed requests in fail[].
 // TODO
-void prio_schedule(request rqs[], request *success[], request *fail[]);
+void prio_schedule(request *rqs[], request *success[], request *fail[]);
