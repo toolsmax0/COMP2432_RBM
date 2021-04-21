cab468dc23845ce3ac1ce515d01a6bbabf768f6f
Eddie Wu <18080998d@gmail.com>
2021-04-21 15:10:33 +0800

    Remade opti.c & opti.h
    Build passed

--- src/lib/opti.c
+++ src/lib/opti.c
@@ -6,62 +6,21 @@ void opti_schedule(request *rqs[],
 				   request *success[],
 				   request *fail[])
 {
-	size_t n_success = 0;
-	size_t n_fail = 0;
-    for (size_t j = 0; j < 4; j++)
-    {
-        if (rqs[j]->priority == j)
-        {
-            for (size_t i = 0; rqs[i] != 0; i++)
-            {
-                if (rqs[i]->isvalid == 0)
-				{
-                    fail[n_fail] = rqs[i];
-                    n_fail++;
-                    continue;
-                }
-
-                if (allocateRoom(rqs[i], success)==0)
-                {
-                    fail[n_fail] = rqs[i];
-                    n_fail++;
-                    continue;
-                }
-
-                if (rqs[i]->device[0][0] == 0)
-                {
-                    success[n_success] = rqs[i];
-                    n_success++;
-                }
-                else
-				{
-                    if (countOccupiedDevice(success,
-											n_success,
-											rqs[i]->device[0],
-											rqs[i]->start,
-											rqs[i]->end)	>=
-							devices[search(rqs[i]->device[0])].quantity		||
-                    	countOccupiedDevice(success,
-											n_success,
-											rqs[i]->device[1],
-											rqs[i]->start,
-											rqs[i]->end)	>=
-							devices[search(rqs[i]->device[1])].quantity)
-                    {
-                        success[n_success] = rqs[i];
-                        n_success++;
-                    }
-                    else
-					{
-                        fail[n_fail] = rqs[i];
-                        n_fail++;
-                    } 
-                }
-            }
-        }
-    }
 	node* init = init_timeline();
+    
+    size_t n_success = 0;
+    size_t n_fail = 0;
 
+    for (size_t i = 0; success[i] != 0; i++)
+    {
+        n_success++;
+    }//count accepted requests
+    
+    for (size_t i = 0; fail[i] != 0; i++)
+    {
+        n_fail++;
+    }//count rejected requests
+    
 	node node_success[n_success];
 	
 	for (size_t i = 0; i < n_success; i++)
@@ -75,5 +34,64 @@ void opti_schedule(request *rqs[],
 	{
 		insert_node(&node_success[i + 1], &node_success[i]);
 	}
-	
+
+    size_t n_resch_date = 0;
+
+    request *resch_date = (request*)calloc(n_fail, sizeof(request));
+    memset(resch_date, 0x00, n_fail * sizeof(request));
+
+    for (size_t i = 0; i < n_fail; i++)
+    {
+        if (fail[i]->isvalid == 1)
+        {
+            resch_date[n_resch_date] = *fail[i];
+            n_resch_date++;
+        }
+    }
+
+    node *node_resch_date[n_resch_date];
+
+    for (size_t i = 0; i < n_resch_date; i++)
+    {
+        node_resch_date[i]->r = &resch_date[i];
+    }
+    
+    node *tmp_init;
+
+    size_t n_resch_time = 0;
+
+    request * resch_time = (request*)calloc(n_resch_date, sizeof(request));
+    memset(resch_time, 0x00, n_resch_date);
+
+    for (size_t i = 0; i < n_resch_date; i++)
+    {
+        tmp_init = search_slot(node_success, resch_date[i].start, resch_date[i].end, 1);
+        if (tmp_init != NULL)
+        {
+            insert_node(node_resch_date[i], tmp_init);
+            tmp_init = NULL;
+        }
+        else
+        {
+            resch_time[n_resch_time] = resch_date[i];
+            n_resch_time++;
+        }
+    }
+    
+    //necessary?
+    node *node_resch_time[n_resch_time];
+
+    for (size_t i = 0; i < n_resch_time; i++)
+    {
+        node_resch_time[i]->r = &resch_time[i];
+    }
+    for (size_t i = 0; i < n_resch_time; i++)
+    {
+        tmp_init = search_time(node_success, resch_time[i].length, 1);
+        if (tmp_init != NULL)
+        {
+            insert_node(node_resch_time[i], tmp_init);
+            tmp_init = NULL;
+        }
+    }
 }
--- src/lib/opti.h
+++ src/lib/opti.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string.h>
+#include <stdlib.h>
+
 #include "request.h"
 #include "timeline.h"
 #include "component.h"
